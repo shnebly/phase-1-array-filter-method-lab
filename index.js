98e1b0b29d305ed51373335a59ccc8b7a41c1cdf
Shelby Morris <shelbynicole.morris@gmail.com>
2023-02-18 16:29:35 -0700

    completed test, imposter syndrome

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+const driversArray = [];
+for(const element of drivers){
+  let driver = element.name;
+  driversArray.push(driver);
+};
+ 
+function findMatching(array, name){
+    const match = array.filter(el => el.toLowerCase() === name.toLowerCase())
+    return match;
+};
+function fuzzyMatch(array, name){
+    const match = array.filter(el => el[0] === name[0])
+    return match;
+};
+function matchName(array, string){
+    const match = array.filter(el => el.name === string)
+    return match;
+};
